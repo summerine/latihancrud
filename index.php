55cbd5f766e9be4bceec8f2494b05199dea26afb
summerine <indri.pr.26@gmail.com>
2019-01-17 17:59:23 +0700

    adding style.css and configure the style

--- index.php
+++ index.php
@@ -8,27 +8,49 @@ include "koneksi.php";
  <html>
  <head>
  	<title>Latihan CRUD</title>
+ 	<link rel="stylesheet" type="text/css" href="style.css">
  </head>
  <body>
+ 	<h1>Latihan CRUD</h1>
+ <div class="container">
  	<form action="tambah.php" method="POST">
- 		<label for="nama">Nama</label><br>
- 		<input type="text" name="nama" required>
- 		<br>
- 		<label for="alamat">Alamat</label><br>
- 		<input type="text" name="alamat" required>
- 		<br>
- 		<label for="no_hp">No. Hp</label><br>
- 		<input type="text" name="no_hp">
- 		<br>
- 		<label for="jenis_kelamin">Jenis Kelamin</label><br>
- 		<input type="radio" name="jenis_kelamin" value="Laki-Laki">Laki-Laki<br>
- 		<input type="radio" name="jenis_kelamin" value="Perempuan">Perempuan<br>
- 		<br>
- 		<br>
+ 		<div class="row">
+ 		<div class="col-25">
+ 			<label for="nama">Nama</label>
+ 		</div>
+ 		<div class="col-75">	
+ 			<input type="text" id="nama" name="nama" placeholder="Nama" required>
+ 		</div>
+ 		</div>
+ 		<div class="row">
+ 		<div class="col-25">	
+ 			<label for="alamat">Alamat</label>
+ 		</div>
+ 		<div class="col-75">
+ 			<textarea id="alamat" name="alamat" placeholder="Alamat" required; style="height: 100px;"> </textarea>
+ 		</div>
+ 		</div>
+ 		<div class="row">
+ 		<div class="col-25">	
+ 			<label for="no_hp">No. Hp</label>
+ 		</div>
+ 		<div class="col-75">
+ 			<input type="text" id="no_hp" name="no_hp" placeholder="No. HP">
+ 		</div>
+ 		</div>
+ 		<div class="row">
+ 		<div class="col-25">
+ 			<label for="jenis_kelamin">Jenis Kelamin</label>
+ 		</div>
+ 		<div class="col-75">
+ 			<input type="radio" name="jenis_kelamin" value="Laki-Laki">Laki-Laki<br>
+ 			<input type="radio" name="jenis_kelamin" value="Perempuan">Perempuan<br>
+ 		</div>
+ 		</div>
  		<button type="reset" name="reset">Reset</button>
  		<input type="submit" name="tambah" value="Tambah">
  	</form>
-
+ </div>
  	<br>
  	<br>
 
